3abefa83f0619cdf0b2bc10c72a64611356900d6
D2MIN <102301865+D2MIN@users.noreply.github.com>
2022-04-28 15:54:22 +0300

    Update 27.py

--- 27.py
+++ 27.py
@@ -3,23 +3,14 @@ list = file.readlines()
 
 book = {}
 m = 10 ** 10
-s = 0
-n = 0
-chet = 0
-col = 5
+chet = n = 0
 for i in list:
-    # if int(list.index(i)) >= (len(list) // 100) * col:
-    #     print(col, '%\n')
-    #     col += 5
-    s = 0
-    test = 1
+    s = test = 0
     for i in list:
-        i = int(i)
-        s += i * n
-        if (n < len(list) // 2) and (test == 1):
-            n += 1
+        s += int(i) * n
+        if (n < len(list) // 2) and (test == 0): n += 1
         else:
-            test = 0
+            test = 1
             n -= 1
     a = list[0]
     list.remove(a)
