f20865772e13232833aec0a86c6e340c83730525
D2MIN <102301865+D2MIN@users.noreply.github.com>
2022-05-03 22:22:18 +0300

    Update Translete.py

--- Translete.py
+++ Translete.py
@@ -6,8 +6,6 @@ file = open('translete.txt', 'r' ,encoding='utf-8')
 data = file.readlines()
 
 print('Запуск программы... \n')
-for i in tqdm( range( 100 ) ):
-    sleep(0.05)
 
 text = ''
 for i in data:
@@ -18,6 +16,13 @@ print('\nЗаписываю файл... \nНе выключайте програ
 
 tts = gTTS(text = text, lang='ru')
 tts.save('file.mp3')
+
+print('          Чтение файла')
+for i in tqdm( range( 100 ) ):
+    sleep(0.05)
+print('          Запись файла')
+for i in tqdm( range( 100 ) ):
+    sleep(0.05)
 
 print("\033[32m{}".format('\nФайл сохранён'))
 print(input(''))
