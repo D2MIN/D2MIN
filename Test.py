5a9f60702b3c6772b5efd6a05ecd4a8bf51d33d1
D2MIN <102301865+D2MIN@users.noreply.github.com>
2022-06-06 17:20:25 +0300

    19-21 Для Вовы

---
+++ Test.py
@@ -0,0 +1,25 @@
+# +1 , *2
+# >= 62 -- win
+# (10,S)
+from functools import lru_cache
+
+def moves(h):
+    a, b = h
+    return (a+1,b), (a,b+1), (a*2,b), (a,b*2)
+
+@lru_cache(None)
+def game(h):
+    if sum(h) >= 62:
+        return 'W'
+    if any(game(m) == 'W' for m in moves(h)):
+        return 'P1'
+    if all(game(m) == 'P1' for m in moves(h)):
+        return 'B1'
+    if any(game(m) == 'B1' for m in moves(h)):
+        return 'P2'
+    if all(game(m) == 'P1' or game(m) == 'P2' for m in moves(h)):
+        return 'B2'
+
+for s in range(1,52):
+    if game((10,s)) != None:
+        print(s, game((10,s)) )
